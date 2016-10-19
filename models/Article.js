a69e66cb76faed3a9e42dd8c7136a86651701c5e
siitwebcluj <paul.brie@scoalainformala.ro>
2016-10-19 16:42:14 +0000

    something new

--- models/Article.js
+++ models/Article.js
@@ -5,6 +5,7 @@ function Article(options){
 }
 
 Article.prototype.update = function(){
+    //this should be only on feature_1 branch
     //not now
     //Do an AJAX req to https://web-7-siitwebcluj.c9users.io/curs22-23-24/articles/update
 }
